37f4b7a2f0a94106c514747b737f7dcaf6309b79
Console and webGui login account <jguru108@gmail.com>
2021-04-11 22:17:46 -0400

    remove marto stuff

--- martor_demo/app/views.py
+++ martor_demo/app/views.py
@@ -14,30 +14,3 @@ def overview_view(request):
     return render(request, 'overview.html')
 
 
-@login_required
-def post_form_view(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save()
-            messages.success(request, '%s successfully saved.' % post.title)
-            return redirect('test_markdownify')
-    else:
-        form = PostForm()
-        context = {'form': form, 'title': 'Post Form'}
-    theme = getattr(settings, 'MARTOR_THEME', 'bootstrap')
-    return render(request, '%s/form.html' % theme, context)
-
-
-def test_markdownify(request):
-    post = Post.objects.last()
-    context = {'post': post}
-    if post is None:
-        context = {
-            'post': {
-                'title': 'Fake Post',
-                'description': """It **working**! :heart: [Python Learning](https://python.web.id)"""
-            }
-        }
-    theme = getattr(settings, 'MARTOR_THEME', 'bootstrap')
-    return render(request, '%s/test_markdownify.html' % theme, context)
