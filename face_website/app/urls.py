dfa11c63c9c6f6884744e33e1f6b139ed3ed1fd4
Swicegood <jguru108@gmail.com>
2021-04-20 22:10:20 -0400

    add favicon.ico

--- face_website/app/urls.py
+++ face_website/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from app.views import (home_redirect_view, overview_view)
 
 urlpatterns = [
     path('', home_redirect_view, name='home_redirect'),
     path('overview/', overview_view, name='overview'),    
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
