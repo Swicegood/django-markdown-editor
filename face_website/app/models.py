e1e374f9e6e3158d1b12d0cfdc122f7255ce03af
Swicegood <jguru108@gmail.com>
2021-04-22 22:36:33 -0400

    change backend

--- face_website/app/models.py
+++ face_website/app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetime import timedelta
 
 
 class Event(models.Model):
     date = models.DateTimeField()
     arotik = models.CharField(max_length=33)
     face = models.CharField(max_length=33)
-
+    ontime = models.BooleanField(null=True)
+    # minlate = models.DurationField(default=timedelta())
--- face_website/face_website/settings.py
+++ face_website/face_website/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'face_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'face',
+        'USER': 'faceuser',
+        'PASSWORD': 'facepass',
+        'HOST': '192.168.0.67',
+        'PORT': '3306',
     }
 }
 
